a89e4c7fefb102452de744daba670a17f37e6571
lamsalbishal <bishalrajlamsal65@gmail.com>
2022-09-06 00:39:43 +0545

    update the stripe

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")('sk_test_51Leix5ETIuVjzRSXtk9pSAXfHuUxIEQwUE57TqSb5vGeAEmNYrsFuzPmpLAauiWg6Z3tAgg7AULvuErX6OPxBWAZ006LtxU7SH');
+const stripe = require("stripe")(
+  "sk_test_51Leix5ETIuVjzRSXtk9pSAXfHuUxIEQwUE57TqSb5vGeAEmNYrsFuzPmpLAauiWg6Z3tAgg7AULvuErX6OPxBWAZ006LtxU7SH"
+);
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -11,20 +13,32 @@ app.use(express.json());
 const apiPort = process.env.PORT || 8000;
 
 app.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
+  let { amount, id } = req.body;
+  try {
+    await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Yammy company",
+      payment_method: id,
+      confirm: true,
+    });
 
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
+    res.status(200).json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
 });
 
-app.listen(apiPort, () => console.log(`Node server listening on port ${apiPort} !`));
+app.get("/test", async (req, res) => {
+  res.json({ message: "success" });
+});
+
+app.listen(apiPort, () =>
+  console.log(`Node server listening on port ${apiPort} !`)
+);
