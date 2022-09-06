64a3a7363f85b221ec076babf9846d20f92a960c
lamsalbishal <bishalrajlamsal65@gmail.com>
2022-09-06 23:25:12 +0545

    update the stripe

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.use(bodyparser.json());
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")(
-  "sk_test_51Leix5ETIuVjzRSXtk9pSAXfHuUxIEQwUE57TqSb5vGeAEmNYrsFuzPmpLAauiWg6Z3tAgg7AULvuErX6OPxBWAZ006LtxU7SH"
-);
+
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -22,7 +20,7 @@ const apiPort = process.env.PORT || 8000;
 
 app.use("/", require("./routes/router"));
 
-app.post("/test",(req,res) => {
+app.get("/test",(req,res) => {
     res.send("i am testing")
 })
 
